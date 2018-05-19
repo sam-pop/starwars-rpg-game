7c9ad3a5f63ef3138cede814cfca0890c8afea56
sam-pop <poplovitch.samuel@gmail.com>
2018-05-19 00:33:37 -0400

    js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -58,7 +58,10 @@ $(document).on('click', 'img', function () {
     }
 });
 
-initCharacters();
+$(document).ready(function () {
+    initCharacters();
+});
+
 
 
 //TODO: REMINDER! use charArr[index] to perform actions like attack etc, since luke,vader,obi etc are declared locally!
