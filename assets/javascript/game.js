fc788e303275d4c069f018b7af62fb4212d67e4c
sam-pop <poplovitch.samuel@gmail.com>
2018-05-19 21:58:49 -0400

    js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 // Global variables
 var originalAttack = 0; // original attack strength
 var player; // holds the player Object
+var defender; // holds the current defender Object
 var charArray = []; // array that stores the game characters (Objects)
 var playerSelected = false; // flag to mark if we picked a player yet
+var defenderSelected = false; // flag to mark if we picked a defender
 
 
 // Constructor
@@ -45,16 +47,28 @@ function setOriginalAttack(Obj) {
 }
 
 // Stores the character the user clicked on in the player variable and removes it from charArray
-$(document).on('click', 'img', function () {
-    if (!playerSelected) {
+$(document).on("click", "img", function () {
+    if (!playerSelected) { // sets current player
         for (var i = 0; i < charArray.length; i++) {
             if (charArray[i].name == (this).id) {
                 player = charArray[i];
+                console.log("​player", player); //TODO: DELETE
+                setOriginalAttack(player);
                 charArray.splice(i, 1);
                 playerSelected = true;
             }
         }
-        $('#playerDiv').append(this);
+        $("#playerDiv").append(this); // appends the selected player to the div
+    }
+    if (playerSelected && !defenderSelected) { // sets defender
+        for (var j = 0; j < charArray.length; j++) {
+            if (charArray[j].name == (this).id) {
+                defender = charArray[j];
+                console.log("​defender", defender); // TODO: DELETE
+                defenderSelected = true;
+            }
+        }
+        $("#defenderDiv").append(this); // appends the selected defender to the div
     }
 });
 
